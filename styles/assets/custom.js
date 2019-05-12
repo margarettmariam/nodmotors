5a32c70c0dc368dd16bee2d091c5b7c8b7b92bf5
margarettmariam <sichinava.95@gmail.com>
2019-05-13 01:28:57 +0400

    booking form

--- styles/assets/custom.js
+++ styles/assets/custom.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
    $(".navbar-toggler").click(function(){
-       $(this).toggleClass("expanded")
-       $(".navigation").toggleClass("expanded")
-   })
+       $(this).toggleClass("expanded");
+       $(".navigation").toggleClass("expanded");
+   });
+
+    $(".book-btn").click(function(){
+        $("body").toggleClass("modal-open");
+    });
+
+    $("form.inputs").submit(function(e){
+        e.preventDefault();
+        $(this).slideUp();
+        $(".success-booking-wrapper").slideDown();
+
+    })
 });
